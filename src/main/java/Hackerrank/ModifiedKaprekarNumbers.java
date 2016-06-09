454182cb23f26eb26fe3bd739d765f985dd855f2
Balram Mirani <balram.mirani@gmail.com>
2016-06-09 12:44:08 +0000

    democommit

--- src/main/java/Hackerrank/ModifiedKaprekarNumbers.java
+++ src/main/java/Hackerrank/ModifiedKaprekarNumbers.java
@@ -16,6 +16,7 @@ public class ModifiedKaprekarNumbers {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Nothing to be added
     }
     
 }
