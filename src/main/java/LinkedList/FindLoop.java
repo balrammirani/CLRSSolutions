4e81dde726ec5632c4216ddef9406c51c0eaf7a6
Balram Mirani <balram.mirani@gmail.com>
2016-06-18 15:49:20 +0530

    FindLoop (Check if a linked list consists a loop)

---
+++ src/main/java/LinkedList/FindLoop.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinkedList;
+
+import static LinkedList.FindMiddle.findMiddle;
+
+/**
+ *
+ * @author Balram
+ */
+public class FindLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static boolean checkLoop(Node head) {
+        Node slowptr = head;
+        Node fastptr = head;
+        while (slowptr != null && fastptr != null && fastptr.getNext() != null) {
+            slowptr = slowptr.getNext();
+            fastptr = fastptr.getNext().getNext();
+            if (slowptr == fastptr) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LinkedListBasics ll = new LinkedListBasics();
+        ll.insertatFirst(1);
+       
+        ll.display();
+        
+          ll.insertafter(1, 20);
+  ll.insertafter(20, 4);
+  ll.insertafter(4, 15);
+  ll.insertafter(15, 10);
+
+  /* Create a loop for testing */
+  ll.head.getNext().getNext().getNext().setNext(ll.head);
+ //Imagine a loop
+  /*
+   1->20->4->15->10---
+   ^                 |
+   |                 |
+   -------------------
+  */
+   System.out.println(checkLoop(ll.head));
+    }
+
+}
